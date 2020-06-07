24c80aef8c353fb27085b4af755eb298fde4944a
natalie-lam <65218346+natalie-lam@users.noreply.github.com>
2020-06-07 12:05:01 +0800

    Add files via upload

--- union.py
+++ union.py
@@ -77,6 +77,7 @@ df.sort_values('pingtimestamp', axis=0, inplace=True, kind='mergesort')
 df['avg_speed'] = df.speed
 df['hour'] = df.pingtimestamp
 df['day'] = df.pingtimestamp
+df['day of apr'] = df.pingtimestamp
 df['is_Weekday'] = df.pingtimestamp
 df['time_group'] = df.pingtimestamp
 df['origin_lat'] = df.rawlat
@@ -91,6 +92,7 @@ df = df.groupby('trj_id', as_index=False).agg({'avg_speed': 'mean',
                                                'osname': 'first',
                                                'hour': lambda x: pd.to_datetime(min(x), unit='s').hour,
                                                'day': lambda x: pd.to_datetime(min(x), unit='s').dayofweek,
+                                               'day of apr': lambda x: pd.to_datetime(min(x), unit='s').day,
                                                'is_Weekday': lambda x: 1 if pd.to_datetime(min(x), unit='s').weekday() < 5 else 0,
                                                'time_group': lambda x: TimeGroup(pd.to_datetime(min(x), unit='s').hour),
                                                'origin_lat': 'first',
@@ -107,7 +109,7 @@ print('Calculating bearing...')
 df['avg_bearing'] = GetDirection(df.origin_lat, df.origin_lng, df.dest_lat, df.dest_lng)
 
 # Checking no duplicates
-print('All IDs are unique: {}'.format(df.trj_id.nunique() == len(df)))
+ArithmeticError(args)print('All IDs are unique: {}'.format(df.trj_id.nunique() == len(df)))
 
 # to_csv
 df.to_csv('all.csv')
